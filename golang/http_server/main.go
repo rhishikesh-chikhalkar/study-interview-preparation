cfdadc64e1073e56b7643edb873abfde8d8410cc
rhishikesh-chikhalkar <rhishichikhalkar21@gmail.com>
2026-07-16 23:28:17 +0530

    feat: initialize basic HTTP server listening on port 8080

---
+++ golang/http_server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from Go")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Server is starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
